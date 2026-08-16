051460b883e8548821fbc6ce3fae7f2a923494ca
Hakkabara <hakkabara@outlook.de>
2026-08-16 22:57:23 +0200

    Extract PipeWire audio module

--- hosts/surf-vm/default.nix
+++ hosts/surf-vm/default.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../modules/nixos/virtaulization/vmware.nix
+      ../../modules/nixos/audio/pipewire.nix
     ];
   # Enable the modern Nix CLI and Flakes.
   nix.settings.experimental-features = [
@@ -78,21 +79,6 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
