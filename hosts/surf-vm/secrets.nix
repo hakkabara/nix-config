eaee16312d2a6ab16c1909805531d1b192aba857
hakkabara <hakkabara@outlook.de>
2026-08-18 06:42:24 +0200

    clean up

--- hosts/surf-vm/secrets.nix
+++ hosts/surf-vm/secrets.nix
@@ -4,8 +4,6 @@
   sops.defaultSopsFile = ../../secrets/surf-vm.yaml;
 
   sops.secrets = {
-    "test-secret" = { };
-
     "ssh-personal-infra" = {
       sopsFile = ../../secrets/shared/ssh-personal-infra;
       format = "binary";
