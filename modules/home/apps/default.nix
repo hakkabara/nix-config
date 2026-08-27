7ec48e29f440a2571416d98dc3b77ce8aef1a27a
hakkabara <hakkabara@outlook.de>
2026-08-27 14:37:48 +0200

    separate SurfVM apps from shared app registry

--- modules/home/apps/default.nix
+++ modules/home/apps/default.nix
@@ -1,86 +1,14 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
+  # Reusable application feature modules.
+  #
+  # Importing this registry should only make application options available.
+  # Profiles decide which applications are actually enabled.
   imports = [
-    ./equibop.nix
     ./browsers
-    ./copyq
     ./flameshot.nix
-    ./immich
     ./pihole
-    ./veracrypt.nix
     ./miniserve
-  ];
-
-  programs = {
-    thunderbird = {
-      enable = true;
-
-      languagePacks = [
-        "de"
-      ];
-
-      policies = {
-        DisableTelemetry = true;
-        DisableAppUpdate = true;
-      };
-    };
-  };
-
-  xdg = {
-    mimeApps = {
-      enable = true;
-
-      associations.added = {
-        "x-scheme-handler/obsidian" = "obsidian.desktop";
-      };
-
-      defaultApplications = {
-        # Web
-        "text/html" = "floorp.desktop";
-        "application/xhtml+xml" = "floorp.desktop";
-        "x-scheme-handler/http" = "floorp.desktop";
-        "x-scheme-handler/https" = "floorp.desktop";
-
-        # PDF
-        "application/pdf" = "org.kde.okular.desktop";
-
-        # E-Books
-        "application/epub+zip" = "koreader.desktop";
-
-        # E-Mail
-        "x-scheme-handler/mailto" = "thunderbird.desktop";
-        "message/rfc822" = "thunderbird.desktop";
-
-        # Obsidian
-        "x-scheme-handler/obsidian" = "obsidian.desktop";
-      };
-    };
-
-    # Some desktop applications rewrite mimeapps.list themselves.
-    # Nix/Home Manager is intentionally the source of truth here.
-    configFile."mimeapps.list".force = true;
-  };
-
-  home.packages = with pkgs; [
-    # Password manager
-    bitwarden-desktop
-
-    # Notes / knowledge
-    obsidian
-
-    # Communication
-    signal-desktop
-    telegram-desktop
-
-    # Media
-    spotify
-    jellyfin-desktop
-
-    # Documents / reading
-    libreoffice-qt-fresh
-    kdePackages.okular
-    koreader
-
   ];
 }
--- profiles/home/surf-vm-apps.nix
+++ profiles/home/surf-vm-apps.nix
@@ -1,7 +1,87 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  # SurfVM-specific application selection.
+  #
+  # Some of these modules are still unconditional legacy modules.
+  # They live here temporarily so importing the shared app registry
+  # no longer installs SurfVM applications on every Home Manager host.
+  #
+  # They can later be converted to reusable enable-based app modules.
   imports = [
+    ../../modules/home/apps/equibop.nix
+    ../../modules/home/apps/copyq
+    ../../modules/home/apps/immich
+    ../../modules/home/apps/veracrypt.nix
     ../../modules/home/apps/element.nix
+  ];
+
+  programs.thunderbird = {
+    enable = true;
+
+    languagePacks = [
+      "de"
+    ];
+
+    policies = {
+      DisableTelemetry = true;
+      DisableAppUpdate = true;
+    };
+  };
+
+  xdg = {
+    mimeApps = {
+      enable = true;
+
+      associations.added = {
+        "x-scheme-handler/obsidian" = "obsidian.desktop";
+      };
+
+      defaultApplications = {
+        # Web
+        "text/html" = "floorp.desktop";
+        "application/xhtml+xml" = "floorp.desktop";
+        "x-scheme-handler/http" = "floorp.desktop";
+        "x-scheme-handler/https" = "floorp.desktop";
+
+        # PDF
+        "application/pdf" = "org.kde.okular.desktop";
+
+        # E-Books
+        "application/epub+zip" = "koreader.desktop";
+
+        # E-Mail
+        "x-scheme-handler/mailto" = "thunderbird.desktop";
+        "message/rfc822" = "thunderbird.desktop";
+
+        # Obsidian
+        "x-scheme-handler/obsidian" = "obsidian.desktop";
+      };
+    };
+
+    # Some desktop applications rewrite mimeapps.list themselves.
+    # Nix/Home Manager is intentionally the source of truth here.
+    configFile."mimeapps.list".force = true;
+  };
+
+  home.packages = with pkgs; [
+    # Password manager
+    bitwarden-desktop
+
+    # Notes / knowledge
+    obsidian
+
+    # Communication
+    signal-desktop
+    telegram-desktop
+
+    # Media
+    spotify
+    jellyfin-desktop
+
+    # Documents / reading
+    libreoffice-qt-fresh
+    kdePackages.okular
+    koreader
   ];
 }
