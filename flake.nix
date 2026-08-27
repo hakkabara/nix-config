0d16329f4852937e0dda4d75fe30444fe0a7e980
hakkabara <hakkabara@outlook.de>
2026-08-27 09:24:36 +0200

    add encrypted SurfVM install target

--- flake.nix
+++ flake.nix
@@ -134,6 +134,35 @@
           ];
         };
 
+        # Installation target for a fresh encrypted real SurfVM.
+        #
+        # This keeps destructive Disko settings separate from the currently
+        # running legacy SurfVM configuration.
+        surf-vm-install = nixpkgs.lib.nixosSystem {
+          inherit system;
+
+          specialArgs = {
+            inherit wl-x11-clipsync;
+          };
+          modules = [
+            ./hosts/surf-vm-install
+            nix-flatpak.nixosModules.nix-flatpak
+            home-manager.nixosModules.home-manager
+            sops-nix.nixosModules.sops
+            disko.nixosModules.disko
+
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                extraSpecialArgs = {
+                  inherit plasma-manager pkgsUnstable zellij-tools;
+                };
+              };
+            }
+          ];
+        };
+
         # Disposable integration target for validating Disko + LUKS2 + Btrfs
         # before migrating the real SurfVM.
         storage-test-vm = nixpkgs.lib.nixosSystem {
